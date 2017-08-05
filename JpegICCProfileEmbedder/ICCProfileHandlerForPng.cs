514438e49a67b39b509638b82b694456f74877c4
Simeon052 <simeon_the_city_who_fought@outlook.com>
2017-08-05 17:59:46 +0900

    Support ICC Profile insert from file

--- JpegICCProfileEmbedder/ICCProfileHandlerForPng.cs
+++ JpegICCProfileEmbedder/ICCProfileHandlerForPng.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace JpegICCProfileEmbedder
 {
@@ -87,20 +89,160 @@ namespace JpegICCProfileEmbedder
 
         }
 
+        static byte[] ICCProfile_Header = { 0x49, 0x43, 0x43, 0x20, 0x50, 0x72, 0x6F, 0x66, 0x69, 0x6C, 0x65, 0x00, 0x00 }; 
+        public static bool Insert_ICCProfile(string srcPath, string ICCProfile)
+        {
+            bool result = false;
+            using (FileStream fsChunkData = new FileStream(ICCProfile, FileMode.Open, FileAccess.Read))
+            { 
+                var chunkDataBuffer = new byte[fsChunkData.Length];
+                fsChunkData.Read(chunkDataBuffer, 0, chunkDataBuffer.Length);
 
-        // CRC32 caliculation is required
-        //public static bool Insert(string srcPath, string ICCProfilePath)
-        //{
-        //    bool result = false;
-        //    using (FileStream fsICCProfile = new FileStream(ICCProfilePath, FileMode.Open, FileAccess.Read))
-        //    {
-        //        var buffer = new byte[fsICCProfile.Length];
-        //        fsICCProfile.Read(buffer, 0, buffer.Length);
-        //        result = Insert(srcPath, buffer);
-        //    }
-        //    return result;
-        //}
+                var compressed = zCompress(chunkDataBuffer);
 
+                var crc = Crc32(Enumerable.Concat(ICCProfile_Header, compressed).ToArray(), ChunkType.iCCP);
+                // CRCの書き込み
+                byte[] crcBuf = BitConverter.IsLittleEndian ? BitConverter.GetBytes(crc).Reverse().ToArray() : BitConverter.GetBytes(crc);
+                result = Insert(srcPath, ChunkDic[ChunkType.iCCP], Enumerable.Concat(ICCProfile_Header, compressed).ToArray(), crcBuf);
+            }
+            return result;
+        }
+
+
+
+        #region https://www.rainorshine.asia/2016/06/05/post2696.html
+
+        public class Adler32 : HashAlgorithm
+        {
+            private const uint BASE = 65521; /* largest prime smaller than 65536 */
+
+            private uint s1;
+            private uint s2;
+
+            public Adler32()
+            {
+                base.HashSizeValue = 32;
+                Initialize();
+            }
+
+            protected override void HashCore(byte[] array, int ibStart, int cbSize)
+            {
+                while (--cbSize >= 0) {
+                    s1 = (s1 + array[ibStart++]) % BASE;
+                    s2 = (s2 + s1) % BASE;
+                }
+            }
+
+            protected override byte[] HashFinal()
+            {
+                HashValue = new byte[] {
+                (byte)((s2 >> 8) & 0xFF),
+                (byte)( s2       & 0xFF),
+                (byte)((s1 >> 8) & 0xFF),
+                (byte)( s1       & 0xFF)
+            };
+                return HashValue;
+            }
+
+            public override void Initialize()
+            {
+                s1 = 1;
+                s2 = 0;
+            }
+        }
+
+        public static byte[] zCompress(byte[] buf)
+        {
+            byte[] cbuf;
+            using (var ms = new MemoryStream())
+            {
+                using (var ds = new DeflateStream(ms, CompressionLevel.Optimal))
+                {
+                    ds.Write(buf, 0, buf.Length);
+                }
+
+                ms.Close();
+                cbuf = ms.ToArray();
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                var zlibHeader = new byte[] { 0x78, 0x9C }; // CMF(CM, CINFO), FLG
+                ms.Write(zlibHeader, 0, zlibHeader.Length);
+
+                ms.Write(cbuf, 0, cbuf.Length);
+
+                var adler32 = new Adler32();
+                var checksum = adler32.ComputeHash(buf);
+                ms.Write(checksum, 0, checksum.Length);
+
+                ms.Close();
+                return ms.ToArray();
+            }
+        }
+        #endregion
+
+        public static bool Insert(string srcPath, string ChunkPath, ChunkType type = ChunkType.iCCP)
+        {
+            bool result = false;
+            using (FileStream fsChunkData = new FileStream(ChunkPath, FileMode.Open, FileAccess.Read))
+            {
+                var chunkDataBuffer = new byte[fsChunkData.Length];
+                fsChunkData.Read(chunkDataBuffer, 0, chunkDataBuffer.Length);
+
+                var crc = Crc32(chunkDataBuffer, type);
+                // CRCの書き込み
+                byte[] crcBuf = BitConverter.IsLittleEndian ? BitConverter.GetBytes(crc).Reverse().ToArray() : BitConverter.GetBytes(crc);
+                result = Insert(srcPath, ChunkDic[type], chunkDataBuffer, crcBuf);
+            }
+            return result;
+        }
+
+        /// <summary>
+        ///  https://stackoverflow.com/questions/24082305/how-is-png-crc-calculated-exactly
+        /// </summary>
+        static uint[] crcTable;
+
+        public static uint Crc32(byte[] stream, ChunkType type)
+        {
+            var chunkTypeCrc = Crc32(ChunkDic[type], 0, ChunkTypeSize, 0);
+
+            return Crc32(stream, 0, stream.Length, chunkTypeCrc);
+        }
+
+        // Stores a running CRC (initialized with the CRC of "IDAT" string). When
+        // you write this to the PNG, write as a big-endian value
+
+        // Call this function with the compressed image bytes, 
+        // passing in idatCrc as the last parameter
+        private static uint Crc32(byte[] stream, int offset, int length, uint crc)
+        {
+
+            uint c;
+            if (crcTable == null)
+            {
+                crcTable = new uint[256];
+                for (uint n = 0; n <= 255; n++)
+                {
+                    c = n;
+                    for (var k = 0; k <= 7; k++)
+                    {
+                        if ((c & 1) == 1)
+                            c = 0xEDB88320 ^ ((c >> 1) & 0x7FFFFFFF);
+                        else
+                            c = ((c >> 1) & 0x7FFFFFFF);
+                    }
+                    crcTable[n] = c;
+                }
+            }
+            c = crc ^ 0xffffffff;
+            var endOffset = offset + length;
+            for (var i = offset; i < endOffset; i++)
+            {
+                c = crcTable[(c ^ stream[i]) & 255] ^ ((c >> 8) & 0xFFFFFF);
+            }
+            return c ^ 0xffffffff;
+        }
 
         /// <summary>
         /// memory上のChunkをIHDRの直後に埋め込む
@@ -117,17 +259,12 @@ namespace JpegICCProfileEmbedder
             {
                 var originalFileSize = fsImage.Length;
                 var originalICCProfileSize = ChunkData.Length;
-                if (ChunkData.Length > 0xFFFF)
-                {
-                    throw new InvalidDataException($"{nameof(ChunkData)} is too large, this is not supported size right now.");
-                }
-
                 fsImage.Seek(0, SeekOrigin.Begin);
 
                 // Memry Streamに保存する
                 fsImage.CopyTo(msImageWithoutHeader);
 
-                // Color profileを埋め込んだ後のファイルサイズにする。
+                // Chunkを埋め込んだ後のファイルサイズにする。
                 var expectedFileSize = fsImage.Length + ChunkType.Length + ChunkData.Length + ChunkCRC.Length;
                 fsImage.SetLength(expectedFileSize);
 
@@ -149,7 +286,7 @@ namespace JpegICCProfileEmbedder
                 // ChunkCRCの書き込み
                 fsImage.Write(ChunkCRC, 0, (int)ChunkCRC.Length);
 
-                // MemoryにあるJpeg fileのSOI, App0以外の部分を書き込み
+                // MemoryにあるIHDRより後のChunkを書き込み
                 var remainImageDataLength = (int)(msImageWithoutHeader.Length - (PNG_SignatureSize + IHDR_ChunkSize));
                 fsImage.Write(msImageWithoutHeader.GetBuffer(), PNG_SignatureSize + IHDR_ChunkSize, remainImageDataLength);
 
@@ -161,7 +298,7 @@ namespace JpegICCProfileEmbedder
 
 
         /// <summary>
-        /// PNG から指定のChunkを取り出す。
+        /// PNG から指定のChunk Data部分をファイルに書き出す。
         /// </summary>
         /// <param name="srcPath">Source file</param>
         /// <param name="ChunkDataPath">Chunk Dataを保存するファイル</param>
@@ -170,10 +307,10 @@ namespace JpegICCProfileEmbedder
         {
             try
             {
-                using (var fsICCProfile = new FileStream(ChunkDataPath, FileMode.Create, FileAccess.Write))
+                using (var fsChunkData = new FileStream(ChunkDataPath, FileMode.Create, FileAccess.Write))
                 {
                     var (data, type, crc, size) = Restore(srcPath);
-                    fsICCProfile.Write(data, 0, data.Length);
+                    fsChunkData.Write(data, 0, data.Length);
                 }
             }
             catch(Exception e)
@@ -191,6 +328,7 @@ namespace JpegICCProfileEmbedder
             {ChunkType.iCCP, iCCP },
             {ChunkType.sRGB, sRGB },
             {ChunkType.pHYs, pHYs }
+            // TODO other chunk support
         };
 
         /// <summary>
@@ -198,7 +336,7 @@ namespace JpegICCProfileEmbedder
         /// </summary>
         /// <param name="srcPath">Source file</param>
         /// <param name="type">Chunk Data</param>
-        /// <returns>ChunkData, ChunkType, ChunkCRC</returns>
+        /// <returns>ChunkData, ChunkType, ChunkCRC, size</returns>
         public static (byte[], byte[], byte[], int) Restore(string srcPath, ChunkType type = ChunkType.iCCP)
         {
             byte[] chunk = ChunkDic[type];
@@ -211,14 +349,15 @@ namespace JpegICCProfileEmbedder
                 // TODO : IDATは先頭のChunkのみ
                 while((b = fsSrcImage.ReadByte()) > -1 )
                 {
-                    System.Diagnostics.Debug.WriteLine($"{fsSrcImage.Position}");
-                    if(b == chunk[0]) // iCCP 1st byte
+                    // System.Diagnostics.Debug.WriteLine($"{fsSrcImage.Position}");
+                    if (b == chunk[0]) // iCCP 1st byte
                     {
                         var ChunkType = new Byte[ chunk.Length];
-                        fsSrcImage.Seek(-1, SeekOrigin.Current); //iCCPの一バイト分もどる
-                        fsSrcImage.Read(ChunkType, 0, ChunkType.Length); // ICC ProfileのIdetifyを比較
+                        fsSrcImage.Seek(-1, SeekOrigin.Current); //一バイト分もどる
+                        fsSrcImage.Read(ChunkType, 0, ChunkType.Length); // Chunk Typeを比較
                         if(System.Linq.Enumerable.SequenceEqual (ChunkType.Take(ChunkTypeSize), chunk))
                         {
+                            // Chunk Typeが一致
                             System.Diagnostics.Debug.WriteLine($"Chunk is found at {fsSrcImage.Position}");
 
                             // Chunk Data Lengh取得
